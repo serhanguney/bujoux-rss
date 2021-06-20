85a30b34603249291c17af9455b768db9cc9e75d
yuivae <gnyserhan@gmail.com>
2021-06-20 18:49:55 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data/bujoux.json";
+import "./App.css";
 
 function App() {
+  console.log(data);
+  const filtered = data.filter(
+    (item) => item.product_url !== "paused" && item.category !== ""
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className="App">
+      <li>{`<SHOP>`}</li>
+      {filtered.map((item, index) => (
+        <ul key={index}>
+          <li>{`<SHOPITEM>`}</li>
+          <li>{`<ITEM_ID>${item.handleId}</ITEM_ID>`}</li>
+          <li>{`<PRODUCTNAME>${item.name}</PRODUCTNAME>`}</li>
+          <li>{`<URL>${item.product_url}</URL>`}</li>
+          <li>{`<IMGURL>${item.productImageUrl.split(";")[0]}</IMGURL>`}</li>
+          <li>{`<PRICE_VAT>${item.price}</PRICE_VAT>`}</li>
+          <li>{`<MANUFACTURER>Bujoux</MANUFACTURER>`}</li>
+          <li>{`<CATEGORYTEXT>Glami.cz | Šperky a hodinky | ${item.category}</CATEGORYTEXT>`}</li>
+          <li>{`<SIZE></SIZE>`}</li>
+          <li>{`<DELIVERY_DATE>0</DELIVERY_DATE>`}</li>
+          <li>{`</SHOPITEM>`}</li>
+        </ul>
+      ))}
+      <li>{`</SHOP>`}</li>
+    </ul>
   );
 }
 
